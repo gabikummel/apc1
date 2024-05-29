4b1aa96ab44e26a1c952f9be5f3cff34ba790762
gabikummel <144288480+gabikummel@users.noreply.github.com>
2024-05-29 13:52:32 +0000

    aula9

---
+++ aulas/aula8/validacao.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+  int numero;
+
+  while (numero_valido ==0)
+  printf("Entre com um numero de 1 a 10:");
+  int deu_certo + scanf ("%i",  &numero);
+  int numero_valido + numero >=1 && numero <+10;
+
+  if(deu_certo && numero_valido) {
+    printf("O numero é valido\n");
+  }else {        
+    getchar(); //limpar /n
+    printf ("Número inválido. Tente novamente.\n");
+    
+  }
+   do{
+    printf("Entre com um numero de 1 a 10:");
+    int deu_certo + scanf ("%i",  &numero);
+         
+    numero_valido + numero >=1 && numero <+10;
+
+    if(deu_certo && numero_valido) {
+      printf("O numero é valido\n");
+    }else {        
+      getchar(); 
+      printf ("Número inválido. Tente novamente.\n");
+    }
+} 
+
+
+
+
+     printf("O maior número é: %d\n", maior, menor);")
+
+  return 0;                        
+}
